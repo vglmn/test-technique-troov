7fc28615752fe5d2f9ac13b505716d882731227e
Giom <labaouici02@gmail.com>
2021-11-22 16:39:04 +0100

    post pour items, put pour l'id de items et delete pour l'id items

--- index.js
+++ index.js
@@ -6,11 +6,33 @@ app.get('/items', (req,res) => {
     res.status(200).json(items)
 })
 
+app.post('/items', (req,res) => {
+    items.push(req.body)
+    res.status(200).json(items)
+})
+
 app.get('/items/:id', (req,res) => {
     const id = parseInt(req.params.id)
     const item = items.find(item => item.id === id)
     res.status(200).json(item)
 })
+
+app.put('/items/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    let item = items.find(item => item.id === id)
+    item.name =req.body.name,
+    item.type =req.body.type,
+    item.price =req.body.price,
+    res.status(200).json(item)
+})
+
+app.delete('/items/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    let item = items.find(item => item.id === id)
+    items.splice(items.indexOf(item),1)
+    res.status(200).json(items)
+})
+
 app.listen(8080, () => {
   console.log('Serveur à l\'écoute')
 })
